a19da11188904cff97a31de32ea2e57fd4f2d0b2
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-04-21 17:10:59 -0400

    WIP: Auth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const nextHttps = require("next-https");
+
+const withHttps = nextHttps({
+  enabled: process.env.NODE_ENV === "development",
+  host: "dev.local",
+  key: "./keysAndCerts/localhost.key",
+  cert: "./keysAndCerts/localhost.crt",
+});
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withHttps(nextConfig);
