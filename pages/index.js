9b9f37dde04aec4b78453f59d6f85cad6f2221b6
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-04-22 08:57:16 -0400

    Changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import MainPage from './MainPage.jsx';
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +13,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.js</code>
         </p>
+        <Link href="/MainPage">Click Here!</Link>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -55,6 +58,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
+          <link href="./ "></link>
           <p
             className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
           >
