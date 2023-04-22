daee1f4166a047159cf02251fa1319d4a52160c9
Xiao Wen Wu <xiaokilledwu@gmail.com>
2023-04-22 14:09:36 -0400

    Add recipe picture

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
-import '@/styles/globals.css'
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-import { wrapper, store } from '../src/store.js'
-import { Provider } from 'react-redux'
+import "/styles/globals.css";
+import React from "react";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { wrapper, store } from "../src/store.js";
+import { Provider } from "react-redux";
 
 function App({ Component, pageProps }) {
   return (
-  <>
-    <Provider store={store}>
-      <UserProvider>
-      <Component {...pageProps} /></UserProvider>
-    </Provider>
-  </>
+    <>
+      <Provider store={store}>
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
+      </Provider>
+    </>
   );
 }
 
