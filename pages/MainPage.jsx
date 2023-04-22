4965dbafd7982377111baa2949cf32aa383bbbe5
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-04-22 08:57:16 -0400

    Changes

---
+++ pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+let MainPage = () => {
+  return (
+    <div>
+      <Link >Home page</Link>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default MainPage;
