8591bd3d26967a5aecc1482069c41ba319d3a192
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-04-22 14:08:06 -0400

    Deleted ts files

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import '../styles/globals.css'
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-import { wrapper, store } from '../src/store.js'
-import { Provider } from 'react-redux'
-=======
 import "/styles/globals.css";
 import React from "react";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 import { wrapper, store } from "../src/store.js";
 import { Provider } from "react-redux";
->>>>>>> 36ff6192d5d29671533222889ef616aa5e0a28df
 
 function App({ Component, pageProps }) {
   return (
