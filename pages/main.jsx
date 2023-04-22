59c62873a8d00921edcfd5afc594734934d414ed
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-04-22 14:05:24 -0400

    working on grid on main page

--- pages/main.jsx
+++ pages/main.jsx
@@ -2,12 +2,21 @@ import Link from 'next/link'
 
 let Main = () => {
   return (
-    <div className="flex flex-col">
-      <p>Hello World</p>
-      <div className="flex justify-center border border-2 h-[300px] w-[80%]">
+    <>
+    <p>main</p>
+    <div className="grid grid-col-6 grid-rows-3">
 
-      </div>
+      <div className="bg-black"></div>
+      <div className="border-2 rounded-lg h-[200px]"></div>
+      <div className="bg-black"></div>
+      <div className="bg-blue-300"></div>
+      <div className="bg-blue-300"></div>
+      <div className="bg-blue-300"></div>
+      <div className="bg-blue-300"></div>
+
+
     </div>
+    </>
   )
 }
 
