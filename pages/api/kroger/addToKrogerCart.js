de4a8c28975f3ab0866e1a96c0bab88e5c40d510
Ermin Sljivo <esljivo1996@gmail.com>
2023-04-24 19:14:52 -0400

    comment out settings object for now

--- pages/api/kroger/addToKrogerCart.js
+++ pages/api/kroger/addToKrogerCart.js
@@ -1,18 +1,18 @@
 
 export default function handler(req, res) {
   if (req.method === 'PUT') {
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://api.kroger.com/v1/cart/add",
-      "method": "PUT",
-      "headers": {
-        "Accept": "application/json",
-        "Authorization": "Bearer {{TOKEN}}",
-      },
-      "processData": false,
-      "data": "{\n  \"items\": [\n     {\n       \"upc\": \"0001200016268\",\n       \"quantity\": \2,\n       \"modality\": \"PICKUP\"\\n      }\n    ]\n }"
-    }
+    // var settings = {
+    //   "async": true,
+    //   "crossDomain": true,
+    //   "url": "https://api.kroger.com/v1/cart/add",
+    //   "method": "PUT",
+    //   "headers": {
+    //     "Accept": "application/json",
+    //     "Authorization": "Bearer {{TOKEN}}",
+    //   },
+    //   "processData": false,
+    //   "data": "{\n  \"items\": [\n     {\n       \"upc\": \"0001200016268\",\n       \"quantity\": \2,\n       \"modality\": \"PICKUP\"\\n      }\n    ]\n }"
+    // }
   }
 
   if (req.method === 'GET') {
