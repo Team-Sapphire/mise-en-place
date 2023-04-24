b0ddde01e33557eb33524ac34a2e114579c73ebc
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-04-24 11:47:11 -0400

    Editing todays recipe component

---
+++ pages/components/main/todaysrecipe.jsx
@@ -0,0 +1,11 @@
+
+let TodaysRecipe = () => {
+  return (
+    <div className="flex justify-between items-center pt-[10%] border-2 h-[100%]">
+      <div className="flex bg-sky-500/100 rounded-lg shadow-lg h-[15%]"><p className="">Todays TodaysRecipe</p></div>
+      <div className="flex bg-sky-500/100 rounded-lg shadow-lg h-[15%] w-[10%]"><button className>Lets Make It</button></div>
+    </div>
+  )
+}
+
+export default TodaysRecipe;
--- pages/main.jsx
+++ pages/main.jsx
@@ -1,11 +1,17 @@
 import Link from 'next/link'
-
+import TodaysRecipe from './components/main/todaysrecipe.jsx';
 let Main = () => {
   return (
-    <div>
-      <Link >Home page</Link>
-      <p>Hello World</p>
+    <div class="grid lg:grid-cols-6 grid-rows-6 gap-5 h-[100%] w-[100%]">
+
+    <div class="col-span-6 row-span-2 p-4 shadow-lg rounded-md">
+      <TodaysRecipe />
     </div>
+
+    <div class="col-span-4 row-span-4 p-4 shadow-lg rounded-md flex items-center justify-center">2</div>
+
+    <div class="col-span-2 row-span-4 p-4 shadow-lg rounded-md flex items-center justify-center">3</div>
+  </div>
   )
 }
 
