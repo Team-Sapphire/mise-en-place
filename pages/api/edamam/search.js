90537b619119c38e4d4df3b6e47b2c577e5b4a8f
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2023-04-27 15:44:41 -0400

    Fix wrongful edits

--- pages/api/edamam/search.js
+++ pages/api/edamam/search.js
@@ -1,16 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default function handler(req, res) {
-  let words = "chicken";
-  axios
-    .get(
-      `https://api.edamam.com/search?q=${words}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}&from=0&to=4`
-    )
-    .then((response) => {
-      res.send(response.data.hits);
-    })
-    .catch((err) => {
-      console.log(err);
-      console.log("eeeeeeeeeeerrrrrrrrrrrroooooooorrrrrr pulling from api");
-    });
-}
+  let words = 'chicken'
+      axios.get(`https://api.edamam.com/search?q=${words}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}&from=0&to=4`)
+      .then(response => {
+        res.send(response.data.hits);
+      }).catch(err => {
+        console.log('eeeeeeeeeeerrrrrrrrrrrroooooooorrrrrr pulling from api');
+      })
+};
