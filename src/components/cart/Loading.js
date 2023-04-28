6a2de5250a098a4d5e1685e5d205d874d18c0671
Ermin Sljivo <esljivo1996@gmail.com>
2023-04-27 21:36:00 -0400

    custom loading waiting for api

--- src/components/cart/Loading.js
+++ src/components/cart/Loading.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import {useState, useEffect} from 'react'
+import { motion, animate } from 'framer-motion'
 
-const Loading = () => {
+const Loading = ({shellRows}) => {
+
   return (
   <>
-    <div>
-      <div className='bg-blue-500 h-12 w-12'></div>
-    </div>
+    {shellRows.map((row, index) => {
+      return (
+        <motion.div animate={{opacity: .4}} transition={{ease: 'easeInOut', repeat: Infinity, repeatDelay: 1}} key={index + row} className='flex flex-col container my-5 h-12 w-[750px] rounded-lg border border-black bg-gray-200 opacity-75'>
+          <motion.div initial={{width: 0}} animate={{width: 750}} transition={{ease: 'easeIn', duration: 10}} className={`bg-blue-500 h-12`}></motion.div>
+        </motion.div>
+      );
+    })}
   </>
   );
 }
