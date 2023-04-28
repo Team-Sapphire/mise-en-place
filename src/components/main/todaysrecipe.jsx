798db768f5a78feebf7b4749696bcb344a52c4bc
Kyle Bradford <kyle.h.bradford@gmail.com>
2023-04-28 11:33:07 -0400

    Changes

--- src/components/main/todaysrecipe.jsx
+++ src/components/main/todaysrecipe.jsx
@@ -4,7 +4,7 @@ let TodaysRecipe = ({todaysRecipe}) => {
 
     <div className="flex justify-between items-center border-2 border-primary rounded-lg h-[100%]">
 
-      <div className="flex rounded-lg pl-10 text-primary"><p className="">Todays Recipe</p></div>
+      <div className="flex rounded-lg pl-10 text-primary text"><p className="">Todays Recipe</p></div>
 
       <Link href={`/recipe/${todaysRecipe}`} ><div className="flex bg-primary text-secondary rounded-lg shadow-lg h-[60px] w-[60]"><button className="">Lets Make It</button></div></Link>
 
